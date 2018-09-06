f8c1cb66078de0b66ed5e0335d95878a3cacdf8a
Srinivas Borra <nborra@apple.com>
2018-09-05 21:45:12 -0700

    comments removed

--- src/main/java/com/test/pwd/controller/CourseApiApp.java
+++ src/main/java/com/test/pwd/controller/CourseApiApp.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CourseApiApp {
 	
 	public static void main(String[] args) {
-		//Application starting point
+		
 		SpringApplication.run(CourseApiApp.class, args);
 	}
 
